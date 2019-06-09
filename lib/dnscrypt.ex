130e8385612524aad855d5d39ade750641249227
Ian Clark <ian@ianleeclark.com>
2019-06-09 20:08:37 +0200

    Sketching out remainder of work in dns request/response lifecycle

--- lib/dnscrypt.ex
+++ lib/dnscrypt.ex
@@ -5,7 +5,7 @@ defmodule Dnscrypt do
 
   alias Dnscrypt.Network
   alias Dnscrypt.Utils.Crypto
-  alias Dnscrypt.Types.Certificate
+  alias Dnscrypt.Types.{Certificate, Response, Query}
 
   # TODO(ian): Determine how this info will be provided
   @client_sk :crypto.strong_rand_bytes(32)
@@ -21,7 +21,12 @@ defmodule Dnscrypt do
              is_number(resolver_port) do
     with {:ok, %Certificate{es_version: algorithm, public_key: resolver_public_key}} <-
            Network.fetch_dns_certificate(resolver_host, resolver_ip, resolver_port),
-         {:ok, shared_key} <- Crypto.derive_shared_key(algorithm, @client_sk, resolver_public_key) do
+         {:ok, shared_key} <-
+           Crypto.derive_shared_key(algorithm, @client_sk, resolver_public_key),
+         {:ok, %Query{} = query} <-
+           Query.new(client_magic, client_pk, client_nonce, encrypted_query),
+         binary_query when is_binary(binary_query) <- Query.to_binary(query),
+         {:ok, x} <- Network.send_tcp_request(resolver_ip, resolver_port, binary_query) do
       {:ok, shared_key}
     end
   end
